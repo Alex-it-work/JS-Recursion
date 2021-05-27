aec41fa189cfe821420f50be3f482cc89a9a73a5
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-05-27 22:28:18 +0300

    Fixed task 2

--- js/index.js
+++ js/index.js
@@ -35,16 +35,40 @@ console.log(result);
 
 // 2. Реализовать вывод в консоль скобок (// bracketWrapper3(3); // => сразу в консоль)
 
-bracketWrapper(10);
+bracketWrapperRec(10);
 
-function bracketWrapper(n) {
-  console.log("brackerWrapperRec(n) :>> ", bracketWrapperRec(n));
-
-  function bracketWrapperRec(n) {
-    return n === 1 ? "()" : `(${bracketWrapperRec(n - 1)})`;
+function bracketWrapperRec(n) {
+  if (n === 0) {
+    return;
   }
+  console.log(`(`);
+  bracketWrapperRec(n - 1);
+  console.log(`)`);
 }
 
 // 3. ** Реализовать функцию аналог flat для массивов. (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat)
 
 // сделаю чуть позже
+
+const arr1 = [1, 2, , 4, 5]; //Метод flat удаляет пустые слоты из массива
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]; //arr4.flat(Infinity); =>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+flat(arr2);
+
+function flat(arr, depth = 1) {
+  const realDepthValue = depthValue(arr);
+  console.log("realdepthValue :>> ", realdepthValue);
+  // рекурсия ушла в бесконечность
+  function depthValue(arr) {
+    debugger;
+    if (arr.filter(argument => Array.isArray(argument)).lengh === 0) {
+      return 0;
+    }
+
+    return (
+      1 + depthValue([].concat(arr.filter(argument => Array.isArray(argument))))
+    );
+  }
+}
