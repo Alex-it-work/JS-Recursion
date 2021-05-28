988cd1aee117ebce85f26dab7a9a3ad96c6ac101
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-05-28 23:15:00 +0300

    Task 3 done

--- js/index.js
+++ js/index.js
@@ -55,19 +55,49 @@ const arr2 = [1, 2, [3, 4, [5, 6]]];
 const arr3 = [1, 2, [3, 4, [5, 6]]];
 const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]; //arr4.flat(Infinity); =>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-flat(arr2);
+console.table("flat(arr4,Infinity) :>> ", flat(arr4, Infinity));
 
 function flat(arr, depth = 1) {
-  const realDepthValue = depthValue(arr); // не знаю зачем я нахожу глубину массива, может это и не пригодится в итоге
-  console.log("realdepthValue :>> ", realdepthValue);
-  // рекурсия ушла в бесконечность.  concat передает не занечение вложенного массива, а сам массив, впринципе он так и должен делать
-  function depthValue(arr) {
-    debugger;
-    if (arr.filter(argument => Array.isArray(argument)).lengh === 0) {
-      return 0;
-    }
-    return (
-      1 + depthValue([].concat(arr.filter(argument => Array.isArray(argument))))
-    );
+  if (depth < 0) {
+    console.log("depth must be a positiv value");
+    return;
   }
+  // debugger;
+  let cumulativeArr = [];
+
+  if (depth === 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === "undefined") {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      const temp = flat(arr[i], depth - 1);
+      cumulativeArr = cumulativeArr.concat(temp);
+    } else {
+      cumulativeArr.push(arr[i]);
+    }
+  }
+  return cumulativeArr;
 }
+
+// flat(arr2);
+
+// function flat(arr, depth = 1) {
+//   const realDepthValue = depthValue(arr); // не знаю зачем я нахожу глубину массива, может это и не пригодится в итоге
+//   console.log("realdepthValue :>> ", realdepthValue);
+//   // рекурсия ушла в бесконечность.  concat передает не занечение вложенного массива, а сам массив, впринципе он так и должен делать
+//   function depthValue(arr) {
+//     debugger;
+//     if (arr.filter(argument => Array.isArray(argument)).lengh === 0) {
+//       return 0;
+//     }
+//     return (
+//       1 + depthValue([].concat(arr.filter(argument => Array.isArray(argument))))
+//     );
+//   }
+// }
